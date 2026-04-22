d0cb41648ce36010e9c1b359d30861b4e2e2b3bb
Syahir <sy4h.0fficial@gmail.com>
2026-04-22 13:19:52 +0800

    Q1.cpp modified

--- Q1.cpp
+++ Q1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int opt;
 int main () {
 do { 
-    int x1, x2, result;
+    int x1, x2, int_result;
     int add, sub, mul, rem;
-    float div;
+    double result, div;
 
     cout << "\n\nSimple calculator\n" << "Enter option\n" << "1)+\n"
     << "2)-\n" << "3)*\n" << "4)/\n" <<  "5) mod(x1,x2)\n" 
@@ -15,6 +16,10 @@ do {
     cout << "\nYour selection -> ";
     cin >> opt;
     
+    if (opt == -1) {
+        return 0;
+    }
+
     cout << "Enter first number: ";
     cin >> x1;
     cout << "Enter second number: ";
@@ -22,13 +27,31 @@ do {
     cout << endl;
 
     if (opt == 1) {
-        result = x1+x2;
-        cout << x1 << " + " << x2 << " = " << result;
+        int_result = x1+x2;
+        cout << x1 << " + " << x2 << " = " << int_result;
     }
 
-    else {
-        break;
+    else if (opt == 2) {
+        int_result = x1 - x2;
+        cout << x1 << " - " << x2 << " = " << int_result;
     }
+
+    else if (opt == 3) {
+        int_result = x1 * x2;
+        cout << x1 << " * " << x2 << " = " << int_result;
+    }
+
+    else if (opt == 4) {
+        result = (double)x1 / x2;
+        cout << x1 << " / " << x2 << " = " << result;
+    }
+
+    else if (opt == 5) {
+        int_result = remainder(x1, x2);
+        cout << x1 << " % " << x2 << " = " << int_result;
+    }
+
 } while (true);
+
     return 0;
 }
